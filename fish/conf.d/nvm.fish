44ef15b111d0e1aa5c9285f3f77b7b70950b61b2
laplace825 <laplace_yongle_he@163.com>
2025-03-09 17:45:29 +0800

    🔥hot fix: support node for fish(just export the PATH variable)

--- fish/conf.d/nvm.fish
+++ fish/conf.d/nvm.fish
@@ -26,3 +26,6 @@ end
 if status is-interactive && set --query nvm_default_version && ! set --query nvm_current_version
     nvm use --silent $nvm_default_version
 end
+
+set -gx PATH $PATH "$HOME/.nvm/versions/node/v23.1.0/bin"
+
